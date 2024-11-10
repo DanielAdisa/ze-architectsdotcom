74f1e7442807bc9b77563431b952705a2922feb6
Your Name <you@example.com>
2024-11-10 17:12:16 +0100

    123

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa6";
 
 import logo from "@/public/Assets/zealogo.png"
 import Image from "next/image"
@@ -12,6 +14,26 @@ import { useEffect, useState } from "react"
  
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    }
+
+    // const handleScroll = (e, id) => {
+
+    //     e.preventDefault();
+    //     const targetElement = document.getElementById(id);
+    //     if (targetElement)  {
+    //         const offsetTop = targetElement.offsetTop - 80;
+    //         window.scrollTo({
+    //             top: offsetTop,
+    //             behavior: "smooth"
+    //         })
+    //     }
+    //     setIsMobileMenuOpen(false)
+    // }
+
   const [header , setHeader] = useState(false);
     const scrollHeader = () => {
         if(window.scrollY >= screen.height*0.1){
@@ -29,7 +51,7 @@ const Navbar = () => {
     },[])
   return (
     <div className={header ? 'z-50 fixed text-black  w-screen h-fit bg-stone-50 transition-all ease-in-out' : 'z-50 fixed transition-all text-stone-50 w-screen h-fit backdrop-blur-md opacity-100 ease-in-out'}>
-        <div className=" max-w-7xl mx-auto flex items-center justify-between ">
+        <div className=" max-w-7xl mx-auto flex  items-center justify-between ">
             <div className=" pt-2 pl-0 w-[40%] flex justify-start p-4">
                 {/* <Image src={logo} height={100} width={150} alt="logo"/> */}
                     <Link className="p-4 hover:text-black/80" href={"/"}>
@@ -39,7 +61,12 @@ const Navbar = () => {
                 </Avatar>
                     </Link>
             </div>
-            <div className="w-[50%] p-5 text-[18px] font-medium   flex justify-between">
+            <div className="lg:hidden p-5">
+                <button onClick={toggleMobileMenu }>
+                    {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+                </button>
+            </div>
+            <div className="w-[50%] p-5 text-[18px] font-medium hidden   md:flex justify-between">
                 <Link className={header ? ' p-4 hover:text-black/60 ' : 'p-4 hover:text-white/60 ' } href={"/about"}><span className="">About</span></Link>
                 <Link className={header ? ' p-4 hover:text-black/60 ' : 'p-4 hover:text-white/60 ' } href={"/"}><span className="">Team</span></Link>
                 <Link className={header ? ' p-4 hover:text-black/60 ' : 'p-4 hover:text-white/60 ' } href={"/projects"}><span className="">Projects</span></Link>
@@ -47,6 +74,15 @@ const Navbar = () => {
                 <Link className={header ? ' p-4 hover:text-black/60 ' : 'p-4 hover:text-white/60 ' } href={"/"}><span className="">Contact</span></Link>
             </div>
         </div>
+        {isMobileMenuOpen && (
+            <div className="w-full space-y-10 p-4 pt-0 text-center backdrop-blur-lg lg:hidden h-fit ">
+                <Link className={header ? ' p-4 hover:text-black/60 w-full ' : 'p-4 hover:text-white/60 ' } href={"/about"}><span className="">About</span></Link><br/>
+                <Link className={header ? ' p-4 hover:text-black/60 w-full ' : 'p-4 hover:text-white/60 ' } href={"/"}><span className="">Team</span></Link><br/>
+                <Link className={header ? ' p-4 hover:text-black/60 w-full ' : 'p-4 hover:text-white/60 ' } href={"/projects"}><span className="">Projects</span></Link><br/>
+                <Link className={header ? ' p-4 hover:text-black/60 w-full ' : 'p-4 hover:text-white/60 ' } href={"/"}><span className="">Insights</span></Link><br/>
+                <Link className={header ? ' p-4 hover:text-black/60 w-full ' : 'p-4 hover:text-white/60 ' } href={"/"}><span className="">Contact</span></Link><br/>
+            </div>
+        )}
     </div>
   )
 }
